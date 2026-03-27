0533aedd45e2a097949af941628e040a450a3781
Sarunya <sarunya46mk@gmail.com>
2026-03-27 15:57:00 +0700

    feat(home): create meta-data and home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { HeroSection } from '@/components/home/hero-section';
+import { FeatureSection } from '@/components/home/feature-section';
+import { homeMetadata } from '@/lib/seo/home-metadata';
+
+export const metadata = homeMetadata;
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <HeroSection />
+      <FeatureSection />
+    </main>
+  );
 }
