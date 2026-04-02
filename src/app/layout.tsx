12c5d8e8bdfd17fa6cc58f4aa0da72b2de20bc39
Sarunya <sarunya46mk@gmail.com>
2026-04-02 22:24:35 +0700

    feat(setup-font): create lib font and use fontVariables in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,9 @@ import Header from '@/components/layout/header';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { fontVariables } from '@/lib/fonts';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -31,7 +21,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${fontVariables} h-full antialiased`}
     >
       <body>
         <ThemeProvider>
